dcc9fdaa6dfa60ddb38c7a388334528bcb163211
LesLacek540 <lpl0004@mix.wvu.edu>
2024-03-03 18:34:14 -0500

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react'; 
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function NavigationBar() {
